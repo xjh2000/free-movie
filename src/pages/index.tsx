30f2afffda14a47268fb51163f2a4811961df052
xjh <934622645@qq.com>
2022-05-07 22:38:10 +0800

    GitHub action test

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -8,21 +8,6 @@ export default function HomePage() {
             <p className="text-4xl ">
                 项目介绍
             </p>
-            <p className="text-2xl">
-                前端技术栈
-            </p>
-            <ul className="text-xl">
-                <li>umi : 运行时的上下文环境</li>
-                <li>ant-design-mobile : UI的呈现</li>
-            </ul>
-            <p className="text-2xl">
-                后端技术栈
-            </p>
-            <ul className="text-xl ">
-                <li>spring : 运行时的上下文环境</li>
-                <li>mongodb : 数据库</li>
-            </ul>
-
         </div>
     );
 }
