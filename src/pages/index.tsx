422a9f0eacad98b4ddf2ffcf3856c590aacf30b6
xjh <934622645@qq.com>
2022-05-08 08:39:06 +0800

    eslint support

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,13 +1,11 @@
 import React from 'react';
 
 export default function HomePage() {
-
-
-    return (
-        <div className="content-center text-center text-textColor h-screen bg-primary space-y-2">
-            <p className="text-4xl ">
-                项目介绍
-            </p>
-        </div>
-    );
+  return (
+    <div className="content-center text-center text-textColor h-screen bg-primary space-y-2">
+      <p className="text-4xl ">
+        项目介绍
+      </p>
+    </div>
+  );
 }
--- src/pages/video/index.tsx
+++ src/pages/video/index.tsx
@@ -1,10 +1,9 @@
 import React from 'react';
 
-
 export default function Page() {
-    return (
-        <div className="content-center h-screen bg-primary">
-            <h1>video</h1>
-        </div>
-    );
+  return (
+    <div className="content-center h-screen bg-primary">
+      <h1>video</h1>
+    </div>
+  );
 }
