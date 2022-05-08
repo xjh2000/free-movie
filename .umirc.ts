69ba46d98b8d3b3f4f28236249ed5132af960c0a
xjh <934622645@qq.com>
2022-05-08 12:22:15 +0800

    pwa support

--- .umirc.ts
+++ .umirc.ts
@@ -1,15 +1,31 @@
+import {GenerateSW} from "workbox-webpack-plugin";
 import {defineConfig} from "umi";
 
+
 export default defineConfig({
-    npmClient: "pnpm",
-    hash: true,
-    proxy: {
-        "/api": {
-            target: "http://localhost:8080",
-            changeOrigin: true,
-        }
-    },
-    tailwindcss: {},
-    plugins: ["@umijs/plugins/dist/tailwindcss"],
+  npmClient: "pnpm",
+  hash: true,
+  proxy: {
+    "/api": {
+      target: "http://localhost:8080",
+      changeOrigin: true,
+    }
+  },
+  favicon: "/favicon.ico",
+  links: [
+    {rel: "icon", href: "/favicon.ico"},
+    {rel: "apple-touch-icon", href: "/logo128.png"},
+    {rel: "manifest", href: "/manifest.json"},
+  ],
+  // @ts-ignore
+  chainWebpack(memo: any) {
+    memo.plugin('workbox').use(GenerateSW, [
+      {
+        swDest: './sw.js',
+      },
+    ]);
+  },
+  tailwindcss: {},
+  plugins: ["@umijs/plugins/dist/tailwindcss"],
 
 });
