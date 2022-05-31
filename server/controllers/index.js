fa2873828388a18c4545101ff9aa00aea42144ec
xjh <934622645@qq.com>
2022-05-31 12:31:59 +0800

    - close workflow , use cloudbase webify to replace + express framework

--- server/controllers/index.js
+++ server/controllers/index.js
@@ -1,3 +1,9 @@
+const ApiController = require('./ApiController');
+const LoginController = require('./LoginController');
+const RuleController = require('./RuleController');
 
 module.exports = {
+  ApiController,
+  LoginController,
+  RuleController,
 };
--- server/services/index.js
+++ server/services/index.js
@@ -1,3 +1,9 @@
+const ApiService = require('./ApiService');
+const LoginService = require('./LoginService');
+const RuleService = require('./RuleService');
 
 module.exports = {
+  ApiService,
+  LoginService,
+  RuleService,
 };
