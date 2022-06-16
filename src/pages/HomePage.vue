59a2f81f9f62925bbf3277eb65dcb0719bf4ad5e
xjh <934622645@qq.com>
2022-06-16 13:23:46 +0800

    add axios support

--- src/pages/HomePage.vue
+++ src/pages/HomePage.vue
@@ -1,10 +1,39 @@
 <script setup lang="ts">
 
-</script>
+import {AuthApiFp, Configuration, UserApi} from "../api";
+import axios from "axios";
+
+function test1() {
+  let userApi = new UserApi();
+  userApi.userGetCurrentUserGet().then(response => {
+    console.log(response);
+  });
+}
+
+async function test() {
+  let configuration = new Configuration();
+  configuration.username = "xjh2000";
+  configuration.password = "666666";
+  configuration.baseOptions = {
+    headers: {
+      "Authorization": "Basic " + btoa(configuration.username + ":" + configuration.password)
+    }
+  };
+  let newVar = AuthApiFp(configuration)
+      .authTokenPost();
+
+  let axiosResponse = await newVar();
+  axios.defaults.headers.common["Authorization"] = "Bearer " + axiosResponse.data;
+  console.log(axiosResponse);
+
+}</script>
 
 <template>
   <div>
     <p>this is home page</p>
+    <button @click="test">get token</button>
+    <br/>
+    <button @click="test1">test1</button>
   </div>
 </template>
 
